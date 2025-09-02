b742601ae6ed1d900349cd03ee7246d2dd050515
Wael Halbawi <waelhalbawi@gmail.com>
2025-09-02 10:54:52 -0700

    core: Provide x86_64 implementation
    This is a rudimentary implementation that takes in a callback to process
    the information from each stack frame. The following suggestions improve
    the quality of the computed backtrace:
    - Use the `-fno-omit-frame-pointer` compilation flag since the
      implementation traverses the linked list of frame pointers to compute
      the backtrace.
    - Use the `-fno-optimize-sibling-calls` to disable tail-call
      optimization (TCO).
    - Mark functions that you think must appear in a backtrace as
      'noinline'. You can also apply this setting all functions using the
      `-fno-inline` compilation flag.
    - Symbol names will only be computed for exported symbols. Link
      application binary with `-rdynamic` to export all symbols.
    
    The implementation is NOT async-signal-safe. This will change in the
    future.

--- test/main.c
+++ test/main.c
@@ -1,12 +1,11 @@
 #include "common.h"
 #include "version_test.h"
 
-int main(int argc, char** argv) {
-    BW_UNUSED(argc);
-    BW_UNUSED(argv);
-
-    int res = version_test();
-    BW_UNUSED(res);
+int main(void) {
+    {
+        int res = version_test();
+        BW_UNUSED(res);
+    }
 
     return 0;
 }
