2ee05e884c7e6e09c314c701e5d3e90cd8dc3f10
Wael Halbawi <waelhalbawi@gmail.com>
2025-09-03 12:58:45 -0700

    core: Add aarch64 support on Linux
    The implementation follows the approach of x86-64: traverse the linked
    list of frame pointers and assume that the return address is pushed onto
    the stack.

--- src/context.c
+++ src/context.c
@@ -1,5 +1,7 @@
 #if defined(__x86_64__)
 #include "context_x64.inc" // IWYU pragma: keep
+#elif defined(__aarch64__)
+#include "context_aarch64.inc" // IWYU pragma: keep
 #else
 #error "unsupported platform"
 #endif // defined(__x86_64__)
