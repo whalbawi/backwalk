c8c71c720aaeb92fe0e4ffc98b9537441bbfead4
Wael Halbawi <waelhalbawi@gmail.com>
2025-12-04 19:57:26 -0800

    sanitizer: Enable Memory Sanitizer
    MSan is not supported by GCC.

--- src/context.h
+++ src/context.h
@@ -1,8 +1,10 @@
 #ifndef BW_CONTEXT_H
 #define BW_CONTEXT_H
 
-#include <stdbool.h>  // for bool
-#include <stdint.h>   // for uintptr_t
+#include <stdbool.h>    // for bool
+#include <stdint.h>     // for uintptr_t
+
+#include "sanitizer.h"  // for MSAN_UNPOISON
 
 enum { CONTEXT_DATA_LEN = 8 };
 
@@ -10,7 +12,13 @@ typedef struct {
     uintptr_t data[CONTEXT_DATA_LEN];
 } context_t;
 
-void context_init(context_t* ctx);
+void context_init_arch(context_t* ctx);
+
+static inline __attribute__((always_inline)) void context_init(context_t* ctx) {
+    context_init_arch(ctx);
+
+    MSAN_UNPOISON(ctx->data, sizeof(ctx->data));
+}
 
 bool context_step(context_t* ctx);
 
